de9b1125400ba9c3930e73febc60550ca3c97654
Kamran <kamran@lasysadmin.com>
2025-06-06 18:56:21 -0700

    logging in tests

--- trading/admin.py
+++ trading/admin.py
@@ -1,6 +1,6 @@
 # trading/admin.py
 from django.contrib import admin
-from .models import UserProfile, Stock, HistoricalPrice, Holding, Transaction
+from .models import UserProfile, Stock
 
 admin.site.register(UserProfile)
 admin.site.register(Stock)
